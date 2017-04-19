0217d424bc8c8599454a6c9cd8f2000410ee89c0
stilobique <aurelienvlt@free.fr>
2017-04-19 15:05:45 +0200

    Refactor panel collision, split the code (previously in init file)

---
+++ views/collision.py
@@ -0,0 +1,21 @@
+import bpy
+
+
+class UCXPanel(bpy.types.Panel):
+    bl_label = "GA Tools"
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "TOOLS"
+    bl_category = "GA Tools"
+
+    def draw(self, context):
+        layout = self.layout
+
+        layout.label(text="UCX")
+        layout.operator("object.generate_bounding_boxes")
+
+def register():
+    bpy.utils.register_class(UCXPanel)
+
+
+def unregister():
+    bpy.utils.unregister_class(UCXPanel)
