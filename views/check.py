606a703d110c71a8656b8fe2bf375f2728b78ce9
stilobique <aurelienvlt@free.fr>
2017-04-19 15:04:50 +0200

    Create a news view panel for check object

---
+++ views/check.py
@@ -0,0 +1,26 @@
+import bpy
+
+
+# -----------------------------------------------------------------------------
+# Checker Object panel
+# -----------------------------------------------------------------------------
+class CheckerObjectsPanel(bpy.types.Panel):
+    bl_label = "Checker Object"
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "TOOLS"
+    bl_category = "GA Tools"
+    bl_context = "objectmode"
+
+
+    def draw(self, context):
+        layout = self.layout
+
+        layout.label("Check all parameter for selected object")
+
+
+def register():
+    bpy.utils.register_class(CheckerObjectsPanel)
+
+
+def unregister():
+    bpy.utils.unregister_class(CheckerObjectsPanel)
