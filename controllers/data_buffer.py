8729230de6d2da477301f97048c012a2b856b070
stilobique <aurelienvlt@free.fr>
2017-04-21 18:22:13 +0200

    Add more function, but need to be clear

--- controllers/data_buffer.py
+++ controllers/data_buffer.py
@@ -8,29 +8,49 @@ class DataBuffer(bpy.types.Operator):
     bl_label = "Paste information buffer"
 
     def execute(self, context):
-        complete_command = """Begin Map
-   Begin Level
-      Begin Actor Class=StaticMeshActor Name=Box001 Archetype=StaticMeshActor'/Script/Engine.Default__StaticMeshActor'
-         Begin Object Class=StaticMeshComponent Name=StaticMeshComponent0 ObjName=StaticMeshComponent0 Archetype=StaticMeshComponent'/Script/Engine.Default__StaticMeshActor:StaticMeshComponent0'
-         End Object
-         Begin Object Name=StaticMeshComponent0
-            StaticMesh=StaticMesh'/Engine/EditorMeshes/EditorCube.EditorCube'
-            RelativeLocation=(X=44.1928,Y=20.0976,Z=0.0)
-            RelativeScale3D=(X=1.0,Y=1.0,Z=1.0)
-            RelativeRotation=(Pitch=0.0,Yaw=0.0,Roll=0.0)
-            CustomProperties
-         End Object
-         StaticMeshComponent=StaticMeshComponent0
-         Components(0)=StaticMeshComponent0
-         RootComponent=StaticMeshComponent0
-         ActorLabel="Box001"
-      End Actor
-   End Level
-Begin Surface
-End Surface
-End Map"""
+        obj = context.active_object
 
-        clipboard.copy(complete_command)
+        if obj is not None:
+            string_data = \
+'Begin Map \n' \
+'   Begin Level \n'\
+'       Begin Actor '\
+        'Class=StaticMeshActor '\
+        'Name=' + obj.name + ' ' \
+        'Archetype=StaticMeshActor'\
+        '\'/Script/Engine.Default__StaticMeshActor\'\n'\
+'           Begin Object Class=StaticMeshComponent '\
+            'Name=StaticMeshComponent0 '\
+            'ObjName=StaticMeshComponent0 ' \
+            'Archetype = StaticMeshComponent'\
+            '\'/Script/Engine.Default__StaticMeshActor:StaticMeshComponent0' \
+                             '\'\n'\
+'           End Object\n'\
+'           Begin Object '\
+            'Name=StaticMeshComponent0 '\
+            'StaticMesh=StaticMesh\'/Engine/EditorMeshes/EditorCube' \
+                             '.EditorCube\' '\
+            'RelativeLocation=(X=' + str(round(obj.location.x, 2)) + ',Y=' + \
+str(round(obj.location.y, 2)) + ',Z=' + str(round(obj.location.z, 2)) + ')\n'\
+            'RelativeScale3D=(X=1.0,Y=1.0,Z=1.0) '\
+            'RelativeRotation=(Pitch=0.0,Yaw=0.0,Roll=0.0) '\
+            'CustomProperties\n' \
+'           End Object\n' \
+'           StaticMeshComponent=StaticMeshComponent0\n' \
+'           Components(0)=StaticMeshComponent0\n' \
+'           RootComponent=StaticMeshComponent0\n' \
+'           ActorLabel=' + obj.name + '\n' \
+'       End Actor\n' \
+'   End Level\n' \
+'Begin Surface\n' \
+'End Surface\n' \
+'End Map'
+            clipboard.copy(string_data)
+
+        else:
+            self.report({'WARNING'}, "Select an object(s).")
+
+            return {'CANCELLED'}
 
         return {'FINISHED'}
 
@@ -46,5 +66,19 @@ def unregister():
 if __name__ == "__main__":
     register()
 
-
-
+#          Begin Object Name=StaticMeshComponent0
+#             StaticMesh=StaticMesh'/Engine/EditorMeshes/EditorCube.EditorCube'
+#             RelativeLocation=(X=44.1928,Y=20.0976,Z=0.0)
+#             RelativeScale3D=(X=1.0,Y=1.0,Z=1.0)
+#             RelativeRotation=(Pitch=0.0,Yaw=0.0,Roll=0.0)
+#             CustomProperties
+#          End Object
+#          StaticMeshComponent=StaticMeshComponent0
+#          Components(0)=StaticMeshComponent0
+#          RootComponent=StaticMeshComponent0
+#          ActorLabel="Box001"
+#       End Actor
+#    End Level
+# Begin Surface
+# End Surface
+# End Map
