9cad18e646d67669015b061fa765f9203ea4b5af
stilobique <aurelienvlt@free.fr>
2017-04-24 11:40:53 +0200

    Update the DataBuffer Ops, now export Name, position, rotation and scale to Unreal Engine 4

--- controllers/data_buffer.py
+++ controllers/data_buffer.py
@@ -1,56 +1,72 @@
 import bpy
 import clipboard
+from math import degrees
 
 
 class DataBuffer(bpy.types.Operator):
-    """Insert a simple description"""
+    """Export data Position, Rotation and Scale of all selected element"""
     bl_idname = "object.data_buffer"
     bl_label = "Paste information buffer"
 
     def execute(self, context):
-        obj = context.active_object
+        objs = context.selected_objects
+        string_data_prefixe = 'Begin Map \n''   Begin Level \n'
+        string_data_suffixe = '   End Level\n''Begin Surface\n''End ' \
+                              'Surface\n''End Map'
+        string_data = ""
 
-        if obj is not None:
-            string_data = \
-'Begin Map \n' \
-'   Begin Level \n'\
-'       Begin Actor '\
-        'Class=StaticMeshActor '\
-        'Name=' + obj.name + ' ' \
-        'Archetype=StaticMeshActor'\
-        '\'/Script/Engine.Default__StaticMeshActor\'\n'\
-'           Begin Object Class=StaticMeshComponent '\
-            'Name=StaticMeshComponent0 '\
-            'ObjName=StaticMeshComponent0 ' \
-            'Archetype = StaticMeshComponent'\
-            '\'/Script/Engine.Default__StaticMeshActor:StaticMeshComponent0' \
-                             '\'\n'\
-'           End Object\n'\
-'           Begin Object '\
-            'Name=StaticMeshComponent0 '\
-            'StaticMesh=StaticMesh\'/Engine/EditorMeshes/EditorCube' \
-                             '.EditorCube\' '\
-            'RelativeLocation=(X=' + str(round(obj.location.x, 2)) + ',Y=' + \
-str(round(obj.location.y, 2)) + ',Z=' + str(round(obj.location.z, 2)) + ')\n'\
-            'RelativeScale3D=(X=1.0,Y=1.0,Z=1.0) '\
-            'RelativeRotation=(Pitch=0.0,Yaw=0.0,Roll=0.0) '\
-            'CustomProperties\n' \
-'           End Object\n' \
-'           StaticMeshComponent=StaticMeshComponent0\n' \
-'           Components(0)=StaticMeshComponent0\n' \
-'           RootComponent=StaticMeshComponent0\n' \
-'           ActorLabel=' + obj.name + '\n' \
-'       End Actor\n' \
-'   End Level\n' \
-'Begin Surface\n' \
-'End Surface\n' \
-'End Map'
-            clipboard.copy(string_data)
+        for element in objs:
+            print(element.name)
+            if element is not None:
+                position_x = str(round(element.location.x * 100, 2))
+                position_y = str(round(element.location.y * -100, 2))
+                position_z = str(round(element.location.z * 100, 2))
+                rotation_pitch = str(round(degrees(element.rotation_euler.x), 2))
+                rotation_yaw = str(round(degrees(element.rotation_euler.y), 2))
+                rotation_roll = str(round(degrees(element.rotation_euler.z), 2))
+                string_data = string_data + \
+    '      Begin Actor '\
+            'Class=StaticMeshActor '\
+            'Name=' + element.name + ' ' \
+            'Archetype=StaticMeshActor'\
+            '\'/Script/Engine.Default__StaticMeshActor\'\n'\
+    '         Begin Object Class=StaticMeshComponent '\
+                'Name=StaticMeshComponent0 '\
+                'ObjName=StaticMeshComponent0 ' \
+                'Archetype=StaticMeshComponent'\
+                '\'/Script/Engine.Default__StaticMeshActor:StaticMeshComponent0' \
+                                 '\'\n'\
+    '         End Object\n'\
+    '         Begin Object '\
+                'Name=StaticMeshComponent0\n'\
+    '            StaticMesh=StaticMesh\'/Engine/EditorMeshes/EditorCube' \
+                                 '.EditorCube\' \n'\
+    '            RelativeLocation=(X=' + position_x + ',Y=' + \
+    position_y + ',Z=' + position_z + ')\n'\
+    '            RelativeScale3D=(X=' + str(round(element.scale.x, 2)) + ',' \
+                                                                     'Y=' + str(round(element.scale.y, 2)) + ',' \
+                                                                      'Z=' + \
+    str(round(element.scale.z, 2)) + ')\n'\
+    '            RelativeRotation=(Pitch=' + rotation_pitch + ',Yaw=' + \
+    rotation_yaw + ',' \
+                                                              'Roll=' + \
+    rotation_roll + ')\n'\
+    '            CustomProperties\n' \
+    '         End Object\n' \
+    '         StaticMeshComponent=StaticMeshComponent0\n' \
+    '         Components(0)=StaticMeshComponent0\n' \
+    '         RootComponent=StaticMeshComponent0\n' \
+    '         ActorLabel="' + element.name + '"\n' \
+    '      End Actor\n' \
 
-        else:
-            self.report({'WARNING'}, "Select an object(s).")
+            else:
+                self.report({'WARNING'}, "Select an object(s).")
 
-            return {'CANCELLED'}
+                return {'CANCELLED'}
+
+        string_complete = string_data_prefixe + string_data \
+                          + string_data_suffixe
+        clipboard.copy(string_complete)
 
         return {'FINISHED'}
 
@@ -65,20 +81,3 @@ def unregister():
 
 if __name__ == "__main__":
     register()
-
-#          Begin Object Name=StaticMeshComponent0
-#             StaticMesh=StaticMesh'/Engine/EditorMeshes/EditorCube.EditorCube'
-#             RelativeLocation=(X=44.1928,Y=20.0976,Z=0.0)
-#             RelativeScale3D=(X=1.0,Y=1.0,Z=1.0)
-#             RelativeRotation=(Pitch=0.0,Yaw=0.0,Roll=0.0)
-#             CustomProperties
-#          End Object
-#          StaticMeshComponent=StaticMeshComponent0
-#          Components(0)=StaticMeshComponent0
-#          RootComponent=StaticMeshComponent0
-#          ActorLabel="Box001"
-#       End Actor
-#    End Level
-# Begin Surface
-# End Surface
-# End Map
