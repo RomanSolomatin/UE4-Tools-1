211aa379ca899eacb4ec99e3942983ade610d011
aurelienvlt@free.fr <aurelienvlt@free.fr>
2017-07-12 16:06:29 +0200

    Change the lightmap operator to use the popup option, no change are avaible WIP

--- controllers/lightmap.py
+++ controllers/lightmap.py
@@ -4,8 +4,16 @@ import bpy
 class LightmapGenerate(bpy.types.Operator):
     """Generate a news UV map and rename it Lightmap"""
     bl_idname = "uv.lightmap_generate"
-    bl_label = "Make a lightmap channel"
+    bl_label = "Generate a lightmap channel"
+    bl_options = {'REGISTER', 'UNDO'}
 
+    @classmethod
+    def poll(cls, context):
+        return context.active_object is not None and \
+               context.active_object.type == 'MESH'
+
+    # use this method to invoke a popup option
+    @staticmethod
     def execute(self, context):
         bpy.ops.mesh.uv_texture_add()
         bpy.context.object.data.uv_textures["UVMap.001"].name = "UV Lightmap"
